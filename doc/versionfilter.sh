c6114f353a0bb9834370f8fc796386791e01dae2
blanton <theblanton@gmail.com>
2008-08-26 17:22:08 -0400

    git-whatchanged ==> git whatchanged

--- doc/versionfilter.sh
+++ doc/versionfilter.sh
@@ -2,4 +2,4 @@
 
 cd ..
 file="${1/$PWD\//}"
-git-whatchanged -n1 --pretty=oneline -- "${file}" | head -n1 | cut -d ' ' -f 1
+git whatchanged -n1 --pretty=oneline -- "${file}" | head -n1 | cut -d ' ' -f 1
